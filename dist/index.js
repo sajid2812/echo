ca45024be1633247b26eb63423465abebf5c7e1c
sajid-edninja <ssk73573@gmail.com>
2025-03-16 18:46:03 +0530

    add socket handler

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log('Hello');
+Object.defineProperty(exports, "__esModule", { value: true });
+const ws_1 = require("ws");
+const wss = new ws_1.WebSocketServer({ port: 8000 });
+//event handler
+wss.on("connection", (socket) => {
+    console.log("user connected");
+    socket.on("message", (e) => {
+        if (e.toString() === "ping") {
+            socket.send("pong");
+        }
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8000 });
+
+//event handler
+wss.on("connection", (socket) => {
+  console.log("user connected");
+
+  socket.on("message", (e) => {
+    if (e.toString() === "ping") {
+      socket.send("pong");
+    }
+  });
+});
